8c7648997d0e68433de578f9ad8098f92b313dcc
Quoc <voquoc0103@gamil.com>
2022-06-01 11:54:53 +0700

    Quan Ly Sinh Vien OOP

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SinhVienOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SinhVien sv1 = new SinhVien(1, "Vo Nguyen Tran Quoc", (float)8.5, (float)9.5);
+            Console.WriteLine("\n{0} {1} {2} {3} {4}", "MaSV", "HoTen", "DiemLT", "DiemTH", "DiemTB");
+            sv1.hienthiSV();
+        }
+    }
+    
+}
---
+++ SinhVien.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinhVienOOP
+{
+    public class SinhVien
+    {
+        private int MaSV;
+        private string HoTen;
+        private float DiemLT;
+        private float DiemTH;
+       
+        public SinhVien()
+        {
+        }
+        
+        public SinhVien(int MaSV, string HoTen, float DiemLT, float DiemTH)
+        {
+            this.MaSV = MaSV;
+            this.HoTen = HoTen;
+            this.DiemLT = DiemLT;
+            this.DiemTH = DiemTH;
+        }
+        public int getMaSV()
+        {
+            return MaSV;
+        }
+        public void setMaSV(int MaSV)
+        {
+            this.MaSV = MaSV;
+        }
+        public String getHoTen()
+        {
+            return HoTen;
+        }
+        public void setHoTen(String HoTen)
+        {
+            this.HoTen = HoTen;
+        }
+        public float getDiemLT()
+        {
+            return DiemLT;
+        }
+        public void setDiemLT(float DiemLT)
+        {
+            if (DiemLT < 0 || DiemLT > 10)
+            {
+                this.DiemLT = 0;
+            }
+            else
+            {
+                this.DiemLT = DiemLT;
+            }
+        }
+        public float getDiemTH()
+        {
+            return DiemTH;
+        }
+        public void setDiemTH(float DiemTH)
+        {
+            if (DiemTH < 0 || DiemTH > 10)
+            {
+                this.DiemTH = 0;
+            }
+            else
+            {
+                this.DiemTH = DiemTH;
+            }
+        }
+        public float tinhDiemTB()
+        {
+            return (DiemLT + DiemTH) / 2;
+        }
+        public void hienthiSV()
+        {
+            Console.WriteLine(this.toString());
+        }
+        public String toString()
+        {
+            return String.Format("{0} {1} {2} {3} {4}", this.MaSV, this.HoTen, this.DiemLT, this.DiemTH, this.tinhDiemTB());
+
+        }
+    }
+}
+    
+
